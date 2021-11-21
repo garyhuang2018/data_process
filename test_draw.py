fc84520d2dbfcf465449a76d8c5965f7b322de21
gary <303136992@qq.com>
2021-11-21 08:23:30 +0800

    pyplot demo

---
+++ test_draw.py
@@ -0,0 +1,6 @@
+# -*- encoding utf-8 -*-
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()  # Create a figure containing a single axes.
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+plt.show()
